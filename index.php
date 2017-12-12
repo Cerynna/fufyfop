05f9c91d20512ccb49da6421c9565c5c3fcbc1eb
Jeremy MARTIN <cerynna@gmail.com>
2017-12-12 19:04:42 +0100

    : sushi:

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($method == "POST") {
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
-    file_put_contents('test.json',$json);
+    file_put_contents('test.json',$json->result);
 
     $messages = [];
     array_push($messages, array(

30039b0602922b0e79cd8f44bb4f30e139e4b863
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 03:58:41 +0100

    :sushi:

--- GameController.php
+++ GameController.php
@@ -73,7 +73,7 @@ class GameController
             $user->setGame($game);
             $key = $database->updateUser($userID, get_object_vars($user) );
         }
-       $this->setGameResponse($key . " - " .  implode(' ',get_object_vars($user)) );
+       $this->setGameResponse($key . " - " .  implode(' ',get_object_vars($userDB)) );
 
 
 

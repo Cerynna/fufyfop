faf5b006d82cb36ed053d59898f4b5347ab6241d
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 03:50:19 +0100

    :sushi:

--- GameController.php
+++ GameController.php
@@ -5,6 +5,8 @@
  * Date: 12/12/17
  * Time: 17:52
  */
+require 'FirebaseConnect.php';
+require 'User.php';
 
 
 class GameController
@@ -46,7 +48,7 @@ class GameController
 
     public function __construct($json)
     {
-       /*$userID = $json->originalRequest->data->user->userId;
+       $userID = $json->originalRequest->data->user->userId;
 
         $allQuery = strtolower($json->result->resolvedQuery);
 
@@ -70,7 +72,7 @@ class GameController
 
             $user->setGame($game);
             $database->updateUser($userID, $user);
-        }*/
+        }
        $this->setGameResponse("LOL");
 
 

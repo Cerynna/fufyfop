49e9751ea00bdbf570905143c70aabcef819a0aa
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 02:49:36 +0100

    :sushi:

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $response = new Controller();
 
 $res = new \stdClass();
 $res->source = "webhook";
+$res->speech = $response->getResponse();
 $res->messages = $response->getResponse();
 $res->contextOut = array();
 echo json_encode($res);

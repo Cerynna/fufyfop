5340163ddc20e2a9be5deecff7c5300f6e011b69
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 06:39:05 +0100

    :sushi:

--- index.php
+++ index.php
@@ -6,4 +6,5 @@ require 'Controller.php';
 $controller = new Controller();
 
 file_put_contents('outJSON.json',$controller->makeRes());
-echo $controller->makeRes();
+
+echo json_encode(file_get_contents('outJSON.json'));

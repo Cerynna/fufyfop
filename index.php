c84434e131770a99a08289610d0244e316a9ad5e
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 02:52:15 +0100

    :sushi:

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ require 'Controller.php';
 $response = new Controller();
 
 
-$res = new \stdClass();
-$res->source = "webhook";
-$res->speech = $response->getResponse();
-$res->messages = $response->getResponse();
-$res->contextOut = array();
-echo json_encode($res);
+
+$response = new \stdClass();
+$response->speech = $response->getResponse();
+$response->displayText = $response->getResponse();
+$response->source = "webhook";
+echo json_encode($response);

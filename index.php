c3a076a77a8c6e57dc005bd6b2de4efe292b74c9
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 02:54:20 +0100

    :sushi:

--- index.php
+++ index.php
@@ -3,12 +3,24 @@ require 'vendor/autoload.php';
 
 require 'Controller.php';
 
-$response = new Controller();
+$requestBody = file_get_contents('php://input');
+$json = json_decode($requestBody);
+
+file_put_contents('test.json',$requestBody);
+$userName = $json->originalRequest->data->user->userId;
+
+$messages = [];
+array_push($messages, array(
+        "type" => "simple_response",
+        "platform" => "google",
+        "textToSpeech" => "qdfbsroibh $userName"
+    )
+);
 
 
-
-$res = new \stdClass();
-$res->speech = $response->getResponse();
-$res->displayText = $response->getResponse();
-$res->source = "webhook";
+$response = new \stdClass();
+$response->source = "webhook";
+$response->speech = "qljgnlsbn";
+$response->messages = $messages;
+$response->contextOut = array();
 echo json_encode($response);

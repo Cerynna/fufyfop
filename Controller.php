31e043476d29d1019912f121755e9578b7b1e5f7
Jeremy MARTIN <cerynna@gmail.com>
2017-12-13 10:02:21 +0100

    :sushi:

--- Controller.php
+++ Controller.php
@@ -46,7 +46,7 @@ class Controller
                 switch ($action) {
                     case 'jouer':
                         $game = new GameController($json);
-                        $this->setRes($game->getGameResponse() . $action);
+                        $this->setRes($game->getGameResponse());
                         break;
                 }
                 if (!empty($context)){
